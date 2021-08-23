d8780f1bd43362d19c07c5d91446bc7bae8e7454
Luis Rangel <luisrangel2202@gmail.com>
2021-08-23 17:10:17 -0600

    [MOD] user name is now exclusive per company

--- extra-addons/extra/multi_company_email/models/res_partner.py
+++ extra-addons/extra/multi_company_email/models/res_partner.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models
 import logging
 _logger = logging.getLogger(__name__)
+from odoo.exceptions import UserError
 
 
 class Partners(models.Model):
@@ -8,20 +9,24 @@ class Partners(models.Model):
 
     email = fields.Char(company_dependent=True)
     company_dependent_email = fields.Char(company_dependent=True)
+    name = fields.Char(store= True, company_dependent=True)
 
     @api.model_create_multi
     def create(self, vals_list):
         partners = super(Partners, self).create(vals_list)
         for partner in partners.filtered(lambda p: p.email):
             email = partner.email
+            name = partner.name
             for company_id in self.env['res.company'].search([]):
                 partner.with_context(force_company=company_id.id).email = email
+                partner.with_context(force_company=company_id.id).name = name
         return partners
 
     def write(self, vals):
-        if 'email' in vals and not self.env.context.get('force_partner_email'):
+        if 'email' in vals or 'name' in vals and not self.env.context.get('force_partner_email'):
             for partner in self.filtered(lambda p: not p.user_ids):
                 for company_id in self.env['res.company'].search([]):
                     partner.with_context(force_company=company_id.id, force_partner_email=True).email = vals.get('email')
+                    partner.with_context(force_company=company_id.id, force_partner_email=True).name = vals.get('name')
         return super(Partners, self).write(vals)
 
